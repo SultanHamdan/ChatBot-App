93db4d37fc0265360979276a8e150eed68e1fed1
EXCELINDIA\shamanthkumar.sv <shamanthkumar.sv@excelindia.com>
2025-05-07 11:26:20 +0530

    botlogic changes

--- src/components/Logic/BotLogic.js
+++ src/components/Logic/BotLogic.js
@@ -10,14 +10,14 @@ class BotLogic {
     const msg = this.createChatBotMessage(text);
     this.setState((prev) => ({
       ...prev,
-      messages: [...prev.messages, msg]
+      messages: [...prev.messages, msg],
     }));
   };
 
   parseMessage = (message) => {
     const lower = message.toLowerCase();
-  
-    // Checks if the user’s message includes any key from the "general" section of the JSON 
+
+    // Check general section
     if (chatbotData.general) {
       for (const key in chatbotData.general) {
         if (lower.includes(key)) {
@@ -25,52 +25,79 @@ class BotLogic {
         }
       }
     }
-  
-    const mainKey = Object.keys(chatbotData).find(key => key !== "general");
+
+    // Identify the main key (excluding 'general')
+    const mainKey = Object.keys(chatbotData).find((key) => key !== 'general');
     const sections = chatbotData[mainKey];
     if (!sections) return this.sendMessage("Sorry, I couldn't find the data.");
-  
+
     const sectionNames = Object.keys(sections);
     const topics = Array.from(
-      new Set(
-        sectionNames.flatMap(name => Object.keys(sections[name]))
-      )
-    ).filter(topic => topic !== "intro");
-  
-    // First check if a section was mentioned
-    for (const section of sectionNames) {
-      if (lower.includes(section)) {
-        this.setState((prev) => ({
-          ...prev,
-          currentTopic: section,
-          messages: [...prev.messages, this.createChatBotMessage(sections[section].intro)]
-        }));
-        return;
-      }
-    }
-  
-    // Now use currentTopic from state
-    this.setState((prev) => {
-      const currentTopic = prev.currentTopic;
-      if (currentTopic && sections[currentTopic]) {
-        for (const topic of topics) {
-          if (lower.includes(topic)) {
-            return {
-              ...prev,
-              messages: [...prev.messages, this.createChatBotMessage(sections[currentTopic][topic])]
-            };
-          }
-        }
-      }
-  
+      new Set(sectionNames.flatMap((name) => Object.keys(sections[name])))
+    ).filter((topic) => topic !== 'intro');
 
+    // Determine if any section is mentioned in the message
+    const mentionedSection = sectionNames.find((section) =>
+      lower.includes(section)
+    );
+
+    // Determine if any topic is mentioned in the message
+    const mentionedTopic = topics.find((topic) => lower.includes(topic));
+
+    // Update state based on identified section and topic
+    this.setState((prev) => {
+      let currentTopic = prev.currentTopic;
+
+      // If a new section is mentioned, update currentTopic
+      if (mentionedSection) {
+        currentTopic = mentionedSection;
+      }
+
+      // If both section and topic are mentioned
+      if (mentionedSection && mentionedTopic) {
+        return {
+          ...prev,
+          currentTopic,
+          messages: [
+            ...prev.messages,
+            this.createChatBotMessage(sections[currentTopic][mentionedTopic]),
+          ],
+        };
+      }
+
+      // If only section is mentioned
+      if (mentionedSection) {
+        return {
+          ...prev,
+          currentTopic,
+          messages: [
+            ...prev.messages,
+            this.createChatBotMessage(sections[currentTopic].intro),
+          ],
+        };
+      }
+
+      // If only topic is mentioned and currentTopic is set
+      if (mentionedTopic && currentTopic && sections[currentTopic]) {
+        return {
+          ...prev,
+          messages: [
+            ...prev.messages,
+            this.createChatBotMessage(sections[currentTopic][mentionedTopic]),
+          ],
+        };
+      }
+
+      // If neither section nor topic is mentioned
       return {
         ...prev,
-        messages: [...prev.messages, this.createChatBotMessage("Sorry, I can't help you with that.")]
+        messages: [
+          ...prev.messages,
+          this.createChatBotMessage("Sorry, I can't help you with that."),
+        ],
       };
     });
   };
-  
 }
 
 export default BotLogic;
