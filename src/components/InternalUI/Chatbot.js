23df1694e21abb08a7fd07477e50e74cf2bdf08e
EXCELINDIA\Md.sultanshaikh <md.sultanshaikh@excelindia.com>
2025-05-08 17:32:41 +0530

    UI Changes

--- src/components/InternalUI/Chatbot.js
+++ src/components/InternalUI/Chatbot.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { Chatbot as ReactChatbot } from 'react-chatbot-kit';
 import 'react-chatbot-kit/build/main.css';
 import config from '../Configuration/config';
+import './Chatbot.css'
 
 const Chatbot = () => {
   
   return (
 
-    <div style={{ maxWidth: "300px" }}>
-      <ReactChatbot 
+    <>
+    <ReactChatbot 
         config={config} 
         actionProvider={config.customActionProvider} 
         messageParser={config.customMessageParser} 
       />
-    </div>
+    </>
   );
 };
 
